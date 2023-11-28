1ac973a5c973ed5300798f87a4fdb0ee58d79e9a
wilson-silva <wilson-gs@hotmail.com>
2023-11-28 15:04:26 -0300

    salvando aula nivelamento jpa/hibernate java ee

---
+++ src/test/java/com/educandoweb/aulajpamaven/AulajpamavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.aulajpamaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulajpamavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
